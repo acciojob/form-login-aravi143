0869b3a4808249eb5a3be23295e0d50353e23c3c
Aravind Chinna <151151715+aravi143@users.noreply.github.com>
2023-12-26 06:08:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function getFormvalue() {
     //Write your code here
 	event.preventDefault();
 	
-    const forms = document.getElementById('form1')
 	const firstName = form.element('fname').value;
 	const lastName = form.element('lname').value;
 	
 	alert(`${firstName} ${lastName}`)
 }
+    const forms = document.getElementById('form1')
 forms.addEventListener('submit',getFormvalue());
